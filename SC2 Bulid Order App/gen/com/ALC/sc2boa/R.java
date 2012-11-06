287bdfeaf6b538bd99dd15e7c4ac7065f242513e
Andrew Long <andrew.long.3001@gmail.com>
2012-11-06 00:41:21 -0600

    dashboard main activity

--- SC2 Bulid Order App/gen/com/ALC/sc2boa/R.java
+++ SC2 Bulid Order App/gen/com/ALC/sc2boa/R.java
@@ -14,29 +14,37 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int ic_menu_name=0x7f020002;
+        public static final int hammer_icon=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int ic_menu_name=0x7f020003;
+        public static final int protoss_icon=0x7f020004;
+        public static final int random_icon=0x7f020005;
+        public static final int sc2_logo_icon=0x7f020006;
+        public static final int terran_eagle_icon=0x7f020007;
+        public static final int terran_icon=0x7f020008;
+        public static final int zerg_icon=0x7f020009;
     }
     public static final class id {
-        public static final int AddBuildButton=0x7f09000c;
         public static final int AddBuildOrderButton=0x7f090005;
-        public static final int AllBuildsButton=0x7f09000b;
         public static final int BuildOrderInstructionsTextArea=0x7f090004;
         public static final int BuildOrderNameLabel=0x7f090000;
         public static final int BuildOrderNameTextField=0x7f090001;
         public static final int BuildOrderTextAreaTextView=0x7f090006;
-        public static final int BuildOrdersList=0x7f09000d;
-        public static final int Menu_adddefaultdata=0x7f090010;
-        public static final int Menu_addinitaldata=0x7f090011;
-        public static final int Menu_debugDB=0x7f09000f;
-        public static final int Menu_deletedb=0x7f090012;
-        public static final int ProtossBuildsButton=0x7f09000a;
+        public static final int BuildOrdersList=0x7f09000e;
+        public static final int Menu_adddefaultdata=0x7f090011;
+        public static final int Menu_addinitaldata=0x7f090012;
+        public static final int Menu_debugDB=0x7f090010;
+        public static final int Menu_deletedb=0x7f090013;
         public static final int RaceLabel=0x7f090002;
-        public static final int TerranBuildsButton=0x7f090009;
-        public static final int ZergBuildsButton=0x7f090008;
+        public static final int btn_add_build=0x7f09000c;
+        public static final int btn_all_builds=0x7f09000b;
+        public static final int btn_donate=0x7f09000d;
+        public static final int btn_protoss_builds=0x7f090009;
+        public static final int btn_terran_builds=0x7f090008;
+        public static final int btn_zerg_builds=0x7f09000a;
         public static final int mainbox=0x7f090007;
-        public static final int menu_settings=0x7f09000e;
+        public static final int menu_settings=0x7f09000f;
         public static final int racespinner=0x7f090003;
     }
     public static final class layout {
@@ -46,6 +54,7 @@ public final class R {
         public static final int activity_display_message=0x7f030003;
         public static final int activity_main=0x7f030004;
         public static final int activity_select_build_order=0x7f030005;
+        public static final int activity_test_main=0x7f030006;
     }
     public static final class menu {
         public static final int activity_add_build_order=0x7f080000;
@@ -54,6 +63,7 @@ public final class R {
         public static final int activity_display_message=0x7f080003;
         public static final int activity_main=0x7f080004;
         public static final int activity_select_build_order=0x7f080005;
+        public static final int activity_test_main=0x7f080006;
     }
     public static final class string {
         public static final int AddBuildOrderButtonString=0x7f050012;
@@ -64,6 +74,7 @@ public final class R {
         public static final int BuildOrderNameString=0x7f050011;
         public static final int BuildOrderString=0x7f05000f;
         public static final int DeleteDBString=0x7f050018;
+        public static final int DonateString=0x7f05001a;
         public static final int ProtossBuildsString=0x7f050004;
         public static final int RaceString=0x7f050010;
         public static final int TerranBuildsString=0x7f050007;
@@ -81,9 +92,11 @@ public final class R {
         public static final int title_activity_display_message=0x7f05000b;
         public static final int title_activity_main=0x7f050002;
         public static final int title_activity_select_build_order=0x7f05000c;
+        public static final int title_activity_test_main=0x7f050019;
     }
     public static final class style {
         public static final int AppTheme=0x7f070000;
+        public static final int DashboardButton=0x7f070001;
     }
     public static final class xml {
         public static final int buildorderxmlschema=0x7f040000;
